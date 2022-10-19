3d543de68e50d363defe07fc64b2ec650d78622d
Илья Золотов <ilyazolotov@MacBook-Pro-Ila.local>
2022-10-19 18:14:26 +0300

    Commit first task

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// 1. которая на вход принимает число и 
+// 2. выдаёт его квадрат (число умноженное на само себя).
+
+// Например: 
+// 4 -> 16
+// -3 -> 9 
+// -7 -> 49
+
+Console.Write ("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine ($"Квадрат числа {number} = {square}");
+
+// double d = 4.566;
+// string str = "Привет";
+// bool b = false;
+
+
